2a0a54864a74aec020fcbc26672a7ee6e59bcc0b
Muhammad Ali <ali.gulzar@wartsila.com>
2022-05-17 00:29:04 +0300

    Add .deta cloud service configuration

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 
 import requests
-from deta import Deta
+from deta import Deta, app
 
 X_RAPIDAPI_KEY = os.environ["X_RAPIDAPI_KEY"]
 DETA_PROJECT_KEY = os.environ["DETA_PROJECT_KEY"]
 
 
+@app.lib.cron()
 def sync_recipes():
     url = "https://random-recipes.p.rapidapi.com/ai-quotes/2199"
     headers = {
@@ -21,7 +22,3 @@ def sync_recipes():
     for recipe in recipes:
         id = recipe.pop("id")
         db.put(recipe, str(id))
-
-
-if __name__ == "__main__":
-    sync_recipes()
